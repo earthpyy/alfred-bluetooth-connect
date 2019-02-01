bb8190cedf584e34fff75ceb95c9743ca410a8b1
Preeti Yuankrathok <preetisatit@gmail.com>
2019-02-01 23:09:19 +0700

    ability to unset, resolve alias

--- alias.py
+++ alias.py
@@ -12,33 +12,88 @@ DIR_PATH = '{}/Library/Application Support/Alfred {}/Workflow Data/{}'.format(ex
 FILE_PATH = DIR_PATH + '/' + FILE_NAME
 
 
-# get query
-query = sys.argv[1]
+# funcions
+def split_set_sentence(sentence):
+    if '>' in query:  # long type
+        split_words = query.split('>')
 
-# verify syntax with ">"
-if '>' in query:  # long type
-    split_words = query.split('>')
+        alias = split_words[0].strip()
+        name = split_words[1].strip()
 
-    alias = split_words[0].strip()
-    name = split_words[1].strip()
+    else:  # short type
+        split_words = query.split(' ')
 
-else:  # short type
-    split_words = query.split(' ')
+        alias = split_words[0]
+        name = ' '.join(split_words[1:])
 
-    alias = split_words[0]
-    name = ' '.join(split_words[1:])
+    return alias, name
 
-# create alias result
-result = '"{}" "{}"\n'.format(alias, name)
+def safe_check_directory(path):
+    try:
+        makedirs(DIR_PATH)
+    except OSError:
+        if not isdir(DIR_PATH):
+            raise
 
-# safe check for directory exist
-try:
-    makedirs(DIR_PATH)
-except OSError:
-    if not isdir(DIR_PATH):
-        raise
+def get_value_from_line(line):
+    separator = '" "'
+    split_line = line.split(separator)
 
-# write to file
-with open(FILE_PATH, 'ab+') as f:
-    f.write(result)
-    sys.stdout.write(result)
+    alias = split_line[0][1:]  # remove left double quote
+    name = split_line[1][:-2]  # remove right double quote and newline
+
+    return alias, name
+
+
+# get argv
+command = sys.argv[1]
+query = sys.argv[2] if len(sys.argv) > 2 else ''
+
+if command == 'set':  # SET ALIAS
+    # verify syntax with ">"
+    alias, name = split_set_sentence(query)
+
+    # create alias result
+    result = '"{}" "{}"\n'.format(alias, name)
+
+    # safe check for directory exist
+    safe_check_directory(DIR_PATH)
+
+    # write to file
+    with open(FILE_PATH, 'ab+') as f:
+        f.write(result)
+        sys.stdout.write('success')
+
+elif command == 'unset':  # UNSET ALIAS
+    # open file
+    try:
+        with open(FILE_PATH, 'rb+') as f:
+            lines = f.readlines()
+            f.seek(0)
+            for line in lines:
+                alias, _ = get_value_from_line(line)
+                # check if alias is match then not write back
+                if alias != query:
+                    f.write(line)
+            f.truncate()
+            sys.stdout.write('success')
+    except FileNotFoundError:
+        pass
+
+elif command == 'resolve':  # RESOLVE ALIAS
+    # open file
+    try:
+        with open(FILE_PATH, 'rb') as f:
+            found = False
+            for line in f:
+                alias, name = get_value_from_line(line)
+                # check if alias is match then get name
+                if alias == query:
+                    sys.stdout.write(name)
+                    found = True
+                    break
+
+            if not found:
+                sys.stdout.write(query)
+    except FileNotFoundError:
+        sys.stdout.write(query)
