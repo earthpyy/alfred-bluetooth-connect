849b666904e64581d052d94b6082c539a9224610
Preeti Yuankrathok <preetisatit@gmail.com>
2019-02-02 00:42:56 +0700

    improve alias.py

--- alias.py
+++ alias.py
@@ -1,31 +1,26 @@
+import os
 import sys
 
 from utils import DIR_PATH, FILE_PATH, split_set_sentence, safe_check_directory, get_value_from_line
 
 
-# get argv
-command = sys.argv[1]
-query = sys.argv[2] if len(sys.argv) > 2 else ''
-
 if command == 'set':  # SET ALIAS
-    alias, name = split_set_sentence(query)
+    alias = os.environ['alias']
+    device_name = os.environ['device_name']
 
-    # check syntax
-    valid = check_set_syntax(alias, name)
+    str_to_write = '"{}" "{}"\n'.format(alias, name)
 
-    if valid:
-        result = '"{}" "{}"\n'.format(alias, name)
+    # safe check for directory exist
+    safe_check_directory(DIR_PATH)
 
-        # safe check for directory exist
-        safe_check_directory(DIR_PATH)
-
-        # write to file
-        with open(FILE_PATH, 'ab+') as f:
-            f.write(result)
-            sys.stdout.write('Alias \'{}\' has been set!'.format(alias))
+    # write to file
+    with open(FILE_PATH, 'ab+') as f:
+        f.write(str_to_write)
+        sys.stdout.write('Alias \'{}\' has been set!'.format(alias))
 
 elif command == 'unset':  # UNSET ALIAS
-    # open file
+    alias = os.environ['alias']
+
     try:
         with open(FILE_PATH, 'rb+') as f:
             lines = f.readlines()
@@ -33,9 +28,9 @@ elif command == 'unset':  # UNSET ALIAS
 
             found = False
             for line in lines:
-                alias, _ = get_value_from_line(line)
+                line_alias, _ = get_value_from_line(line)
                 # check if alias is match then not write back
-                if alias != query:
+                if alias != line_alias:
                     f.write(line)
                 else:
                     found = True
